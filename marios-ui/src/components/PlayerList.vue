137704a1f57cd30c43ecb758d789a8566501d22b
Tim Knickerbocker <tknick2@protonmail.com>
2024-07-11 22:40:33 -0600

    fixed broken build, I think...

--- marios-ui/src/components/PlayerList.vue
+++ marios-ui/src/components/PlayerList.vue
@@ -9,35 +9,6 @@ const props = defineProps({
     required: true
   }
 })
-
-function submitScores() {
-  try {
-    //backupScores();
-
-    if (parseFloat(p1RaceScore.value)) p1Score.value += parseFloat(p1RaceScore.value)
-
-    if (parseFloat(p2RaceScore.value)) p2Score.value += parseFloat(p2RaceScore.value)
-
-    if (parseFloat(p3RaceScore.value)) p3Score.value += parseFloat(p3RaceScore.value)
-
-    if (parseFloat(p4RaceScore.value)) p4Score.value += parseFloat(p4RaceScore.value)
-
-    if (parseFloat(p5RaceScore.value)) p5Score.value += parseFloat(p5RaceScore.value)
-
-    if (parseFloat(p6RaceScore.value)) p6Score.value += parseFloat(p6RaceScore.value)
-
-    if (parseFloat(p7RaceScore.value)) p7Score.value += parseFloat(p7RaceScore.value)
-
-    if (parseFloat(p8RaceScore.value)) p8Score.value += parseFloat(p8RaceScore.value)
-
-    teamMarioScore.value = p1Score.value + p2Score.value + p3Score.value + p4Score.value
-    teamLuigiScore.value = p5Score.value + p6Score.value + p7Score.value + p8Score.value
-
-    resetRaceScores()
-  } catch (e) {
-    console.log(e)
-  }
-}
 </script>
 
 <template>
